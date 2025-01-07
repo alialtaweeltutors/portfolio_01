55764be21c3a8dcd2d7b77847c7bdf04a05ee81d
AliAlTaweel <engalihaitham@gmail.com>
2025-01-07 18:52:35 +0200

    Footer created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero';
 import Projects from './components/Projects';
 import About from './components/About';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 import './styles/main.css';
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
       <Projects />
       <About />
       <Contact />
+      <Footer />
     </>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../styles/Footer.css"
+
+const Footer = () => {
+  return (
+    <section id="footer" className="footer">
+      <p>
+      Developed and designed by <span>Ali Al-Taweel</span> 2025
+      </p>
+    </section>
+  );
+};
+
+export default Footer;
